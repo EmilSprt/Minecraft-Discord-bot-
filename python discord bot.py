b438c924868399ebbc9e3742fbc5dba3594ca0bc
EmilSprt <92858112+EmilSprt@users.noreply.github.com>
2022-02-13 18:30:43 +0100

    Update python discord bot.py

--- python discord bot.py
+++ python discord bot.py
@@ -1,49 +1,167 @@
+import asyncio
+from discord.ext import commands
+import random
+import os
 import discord
 from mcstatus import MinecraftServer
 from mcipc.query import Client
 
-serverip = "Your ip"
-serverport = "Your Port"
+PlayerListPath = ""
+plugin_path = ""
+thumbnail = ""
+IP = "DEINE IP"
+PORT = "DEIN PORT"
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        while True:
-            server = MinecraftServer.lookup(serverip)
-            status = server.status()
-            await client.change_presence(activity=discord.Game(name=f"on server are | {(status.players.online)} player online"))
-                   
-    async def on_message(self, message):
-        if message.content.startswith('!ip'):
-            await message.channel.send("The ip is Your Ip")
 
-        if message.content.startswith('!state'):
-            try:
-                with Client((serverip), int(serverport), timeout=1.5) as client:
-                    basic_stats = client.stats()
-                    await message.channel.send(basic_stats.motd + ' is online! With ' + str(basic_stats.num_players) + ' out of ' + str(basic_stats.max_players) + ' players.')
+client = discord.Client()
 
-            except:
-                await message.channel.send('Server is offline or no player are online!')
 
-        if message.content.startswith('!list'):
-            try:
-                with Client(serverip, int(serverport), timeout=1.5) as client:
-                    full_stats = client.stats(full=True)
-                    player_list_message = "Player Liste: \n"
-                    for player_name in full_stats.players:
-                        player_list_message = player_list_message + "- " + player_name + "\n"
-
-                    await message.channel.send(player_list_message)
-
-            except:
-                await message.channel.send('Server is offline or no player are online!')
-                
-        if message.content.startswith('!help'):
-            await message.channel.send("ip:                                        !ip")
-            await message.channel.send("amount of player:          !state")
-            await message.channel.send("list of player:  !list")
+@client.event
+async def on_ready():
+        
+        print('Bot logged on!')
             
-client = MyClient()
-client.run("Your Token")
+        while True:
+            server = MinecraftServer.lookup(IP)
+            status = server.status()
+            await client.change_presence(activity=discord.Game(name=f"{(status.players.online)} Spieler online. | Ping: {(status.latency)} |"))
+            await asyncio.sleep(1)
 
+@client.event
+async def on_message(message):
+    
+    if str(message.channel) == "commands":  # bot commands will only get recognized when you write sth in a channel called commands
+
+        if message.content.startswith("/help"):
+            
+            embed = discord.Embed(
+                title  = "Hilfe",
+                color = 0xA9D6EB
+
+            )
+            embed.add_field(name="Server Ip:",value="/ip",inline=False)
+            embed.add_field(name="Player Online:",value="/state",inline=False)
+            embed.add_field(name="Player list:",value="/list",inline=False)
+            embed.add_field(name="Server Plugins:",value="/plugin",inline=False)
+            embed.add_field(name="Playtime:",value="/playtime (Player Name)",inline=False)
+
+
+            embed.set_thumbnail(url=thumbnail)
+
+            await message.channel.send(embed=embed)
+            await asyncio.sleep(1)
+
+
+        if message.content.startswith('/ip'):
+            embed4 = discord.Embed(
+                    title  = "Netzwerk Information",
+                    color = 0xA9D6EB
+                    )
+            embed4.set_thumbnail(url=thumbnail)
+
+           
+
+            embed4.add_field(name="Vereinfachte Domain",value=IP ,inline=False)
+
+
+                
+            await message.channel.send(embed=embed4)
+            
+
+        if message.content.startswith('/state'):
+            try:
+                with Client((IP), int(PORT), timeout=1.5) as client:
+                    basic_stats = client.stats()
+                    embed3 = discord.Embed(title  = "Status", color = 0xA9D6EB)
+                        
+                    embed3.set_thumbnail(url=thumbnail)
+
+                    embed3.add_field(name="Player online",value=str(basic_stats.num_players),inline=False)
+                    embed3.add_field(name="Max. PLayer",value=str(basic_stats.max_players),inline=False)
+                    await message.channel.send(embed=embed3)
+
+            except:
+                await message.channel.send('Server is offline!')
+                    
+        if message.content.startswith('/list'):
+            try:
+                with Client((IP), int(PORT), timeout=1.5) as client:
+                    full_stats = client.stats(full=True)
+                    basic_stats = client.stats
+                    player_list_message = " "
+                    for player_name in full_stats.players:
+                        player_list_message = player_list_message + "- " + player_name + " "
+                        
+
+                    embed2 = discord.Embed(
+                    title  = "Liste",
+                    color = 0xA9D6EB
+                    )
+                    embed2.set_thumbnail(url=thumbnail)            
+
+                    embed2.add_field(name="Spieler online",value=player_list_message,inline=False)
+                    await message.channel.send(embed=embed2)
+
+            except:
+                await message.channel.send('Server is offline !')
+
+
+            
+        if message.content.startswith('/plugin'):                               #list the plugins of the plugin path
+            included_extensions = ['jar']
+            file_names = [fn for fn in os.listdir(plugin_path)
+                        if any(fn.endswith(ext) for ext in included_extensions)]
+                
+            embed1 = discord.Embed(
+            title  = "Plugins",
+            color = 0xA9D6EB
+            )
+                
+            embed1.set_thumbnail(url=thumbnail)            
+            count = 0 
+            for i in file_names:
+                total = (file_names[count])
+                count += 1
+                embed1.add_field(name=count,value=total,inline=False)
+            await message.channel.send(embed=embed1)
+
+
+
+        if message.content.startswith("/playerstats"):                          #gives you the playtime of player on your server.
+            Spieler = message.content.replace("/playerstats ", "")              #only works when you have a minecraft server plugin for it
+            f = open(PlayerListPath)
+            if Spieler in (f.read()):
+                with open(PlayerListPath) as f:
+                    for num, line in enumerate(f, 0):
+                        if Spieler in line:
+                            line = num + 1 
+                            Playtime = f.readlines(line)
+                            line2 = num + 2
+                            Playtime2 = f.readlines(line2)
+                            
+                        
+                    Playtime = str(Playtime).replace("[", "")
+                    Playtime = str(Playtime).replace("]", "")
+                    Playtime = str(Playtime).replace("'", "")
+
+                    Playtime2 = str(Playtime2).replace("[", "")
+                    Playtime2 = str(Playtime2).replace("]", "")
+                    Playtime2 = str(Playtime2).replace("'", "")
+
+                    if "-" in Playtime2:
+                        await message.channel.send(Playtime) 
+                    
+                    else:
+                        await message.channel.send(Playtime)      
+                        await message.channel.send(Playtime2)
+
+
+            else:
+                await message.channel.send(f"{Spieler} wasn't online yet!" )      
+    
+
+                    
+
+
+
+client.run("YOUR TOKEN", bot = True)
